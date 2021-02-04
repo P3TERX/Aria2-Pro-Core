c610ea25fba9d7c4d33c5262606818d8f7f947d5
P3TERX <25927179+P3TERX@users.noreply.github.com>
2021-02-04 17:09:48 +0800

    aria2-install.sh: 2.0

--- aria2-install.sh
+++ aria2-install.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2020 P3TERX <https://p3terx.com>
+# Copyright (c) 2020-2021 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the GNU General Public License v3.
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Aria2-Pro-Core
 # File name: aria2-install.sh
-# Description: Install latest version Aria2
-# System Required: Debian/Ubuntu or other
-# Version: 1.5
+# Description: Install latest version Aria2 Pro Core
+# System Required: GNU/Linux
+# Version: 2.0
 #
 
-set -e
-[ $(uname) != Linux ] && {
-    echo "This operating system is not supported."
-    exit 1
-}
-[ $EUID != 0 ] && SUDO=sudo
-$SUDO echo
+set -o errexit
+set -o errtrace
+set -o pipefail
+set -o nounset
+
 Green_font_prefix="\033[32m"
 Red_font_prefix="\033[31m"
 Green_background_prefix="\033[42;37m"
@@ -26,54 +24,63 @@ Red_background_prefix="\033[41;37m"
 Font_color_suffix="\033[0m"
 INFO="[${Green_font_prefix}INFO${Font_color_suffix}]"
 ERROR="[${Red_font_prefix}ERROR${Font_color_suffix}]"
-ARCH=$(uname -m)
-[ $(command -v dpkg) ] &&
-    dpkgARCH=$(dpkg --print-architecture | awk -F- '{ print $NF }')
 
-echo -e "${INFO} Check CPU architecture ..."
-if [[ $ARCH == i*86 || $dpkgARCH == i*86 ]]; then
-    ARCH="i386"
-elif [[ $ARCH == "x86_64" || $dpkgARCH == "amd64" ]]; then
-    ARCH="amd64"
-elif [[ $ARCH == "aarch64" || $dpkgARCH == "arm64" ]]; then
-    ARCH="arm64"
-elif [[ $ARCH == "armv7l" || $dpkgARCH == "armhf" ]]; then
-    ARCH="armhf"
-else
-    echo -e "${ERROR} This architecture is not supported."
+PROJECT_NAME='Aria2 Pro Core'
+GH_API_URL='https://api.github.com/repos/P3TERX/Aria2-Pro-Core/releases/latest'
+BIN_DIR='/usr/local/bin'
+BIN_NAME='aria2c'
+BIN_FILE="${BIN_DIR}/${BIN_NAME}"
+
+if [[ $(uname -s) != Linux ]]; then
+    echo -e "${ERROR} This operating system is not supported."
     exit 1
 fi
 
-echo -e "${INFO} Get Aria2 download link ..."
-TAG_NAME=$(curl -fsSL https://api.github.com/repos/P3TERX/Aria2-Pro-Core/releases/latest | grep -o '"tag_name": ".*"' | head -n 1 | cut -d'"' -f4)
-[ -z $TAG_NAME ] && {
-    echo -e "${ERROR} Unable to get Aria2 download link, network failure or API error."
+if [[ $(id -u) != 0 ]]; then
+    echo -e "${ERROR} This script must be run as root."
     exit 1
-}
-ARIA2_VER=${TAG_NAME%_*}
-BUILD_DATE=${TAG_NAME#*_}
+fi
 
-echo -e "${INFO} Download Aria2 ${ARIA2_VER} (build ${BUILD_DATE} ${ARCH}) ..."
-curl -L "https://github.com/P3TERX/Aria2-Pro-Core/releases/download/${TAG_NAME}/aria2-${ARIA2_VER}-static-linux-${ARCH}.tar.gz" | tar -xz
-[ ! -s aria2c ] && {
-    echo -e "${ERROR} Unable to download aria2, network failure or other error."
+echo -e "${INFO} Get CPU architecture ..."
+if [[ $(command -v apk) ]]; then
+    PKGT='(apk)'
+    OS_ARCH=$(apk --print-arch)
+elif [[ $(command -v dpkg) ]]; then
+    PKGT='(dpkg)'
+    OS_ARCH=$(dpkg --print-architecture | awk -F- '{ print $NF }')
+else
+    OS_ARCH=$(uname -m)
+fi
+case ${OS_ARCH} in
+*86)
+    FILE_KEYWORD='i386'
+    ;;
+x86_64 | amd64)
+    FILE_KEYWORD='amd64'
+    ;;
+aarch64 | arm64)
+    FILE_KEYWORD='arm64'
+    ;;
+arm*)
+    FILE_KEYWORD='armhf'
+    ;;
+*)
+    echo -e "${ERROR} Unsupported architecture: ${OS_ARCH} ${PKGT}"
     exit 1
-}
+    ;;
+esac
+echo -e "${INFO} Architecture: ${OS_ARCH} ${PKGT}"
 
-while [ $(command -v aria2c) ]; do
-    echo -e "${INFO} Remove old version ..."
-    $SUDO rm -rf $(command -v aria2c) || {
-        echo -e "${ERROR} Unable to remove old version aria2 !"
-        exit 0
-    }
-done
+echo -e "${INFO} Get ${PROJECT_NAME} download URL ..."
+DOWNLOAD_URL=$(curl -fsSL ${GH_API_URL} | grep 'browser_download_url' | cut -d'"' -f4 | grep "${FILE_KEYWORD}")
+echo -e "${INFO} Download URL: ${DOWNLOAD_URL}"
 
-echo -e "${INFO} Installing Aria2 ${ARIA2_VER} (build ${BUILD_DATE} ${ARCH}) ..."
-$SUDO mv aria2c /usr/local/bin &&
-    echo -e "${INFO} Aria2 installed successfully !" ||
-    {
-        echo -e "${ERROR} Aria2 installation failed !"
-        exit 1
-    }
-
-exit 0
+echo -e "${INFO} Installing ${PROJECT_NAME} ..."
+curl -LS "${DOWNLOAD_URL}" | tar xzC ${BIN_DIR}
+chmod +x ${BIN_FILE}
+if [[ -s ${BIN_FILE} && $(${BIN_NAME} -v) ]]; then
+    echo -e "${INFO} Done."
+else
+    echo -e "${ERROR} ${PROJECT_NAME} installation failed !"
+    exit 1
+fi
